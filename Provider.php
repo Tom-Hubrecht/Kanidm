b87d75b8342e00c46ef1c29c42e92b629bb206b1
Tom Hubrecht <Tom-Hubrecht@users.noreply.github.com>
2024-02-18 15:12:08 +0100

    Update Provider.php

--- Provider.php
+++ Provider.php
@@ -72,7 +72,7 @@ class Provider extends AbstractProvider
     {
         return (new User())->setRaw($user)->map([
             'id'       => $user['sub'],
-            'nickname' => $user['nickname'],
+            'nickname' => $user['preferred_username'],
             'name'     => Arr::get($user, 'given_name', '').' '.Arr::get($user, 'family_name', ''),
             'email'    => $user['email'],
             'avatar'   => null,
